043222e800624c1ff80a894121f6489a36891ce9
Roksansa <roksan.girl@rambler.ru>
2021-11-23 03:23:57 +0700

    [Refacting] Spaces to tabs

--- Source/Origin/Characters/Controllers/OPlayerController.cpp
+++ Source/Origin/Characters/Controllers/OPlayerController.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "OPlayerController.h"
 
 #include "OTypes.h"
@@ -10,17 +9,17 @@
 
 void AOPlayerController::SetPawn(APawn* InPawn)
 {
-    Super::SetPawn(InPawn);
-    CachedBaseCharacter = Cast<AOBaseCharacter>(InPawn);
-    if (CachedBaseCharacter.IsValid() && CachedBaseCharacter->IsA<AOPlayerCharacter>())
-    {
-        const AOPlayerCharacter* PlayerCharacter = Cast<AOPlayerCharacter>(CachedBaseCharacter.Get());
-        if (PlayerCameraManager)
-        {
-            PlayerCameraManager->ViewPitchMin = PlayerCharacter->GetViewPitchMin();
-            PlayerCameraManager->ViewPitchMax = PlayerCharacter->GetViewPitchMax();
-        }      
-    }
+	Super::SetPawn(InPawn);
+	CachedBaseCharacter = Cast<AOBaseCharacter>(InPawn);
+	if (CachedBaseCharacter.IsValid() && CachedBaseCharacter->IsA<AOPlayerCharacter>())
+	{
+		const AOPlayerCharacter* PlayerCharacter = Cast<AOPlayerCharacter>(CachedBaseCharacter.Get());
+		if (PlayerCameraManager)
+		{
+			PlayerCameraManager->ViewPitchMin = PlayerCharacter->GetViewPitchMin();
+			PlayerCameraManager->ViewPitchMax = PlayerCharacter->GetViewPitchMax();
+		}
+	}
 }
 
 bool AOPlayerController::IsPressedAnyKeyForAction(const FName ActionName) const
@@ -50,24 +49,24 @@ bool AOPlayerController::FirstPressedKeyForAction(const FName ActionName, FText&
 
 void AOPlayerController::SetupInputComponent()
 {
-    Super::SetupInputComponent();
-    InputComponent->BindAxis("MoveForward", this, &AOPlayerController::MoveForward);
-    InputComponent->BindAxis("MoveRight", this, &AOPlayerController::MoveRight);
-    InputComponent->BindAxis("Turn", this, &AOPlayerController::Turn);
-    InputComponent->BindAxis("LookUp", this, &AOPlayerController::LookUp);
-    InputComponent->BindAxis("TurnAtRate", this, &AOPlayerController::TurnAtRate);
-    InputComponent->BindAxis("LookUpAtRate", this, &AOPlayerController::LookUpAtRate);
-    InputComponent->BindAxis("SwimRight", this, &AOPlayerController::MoveSwimRight);
-    InputComponent->BindAxis("SwimUp", this, &AOPlayerController::MoveSwimUp);
-    InputComponent->BindAxis("SwimForward", this, &AOPlayerController::MoveSwimForward);
-    InputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &AOPlayerController::Jump);
-    InputComponent->BindAction("Crawl", EInputEvent::IE_Pressed, this, &AOPlayerController::ChangeCrawlState);
-    InputComponent->BindAction("Crouch", EInputEvent::IE_Pressed, this, &AOPlayerController::ChangeCrouchState);
-    InputComponent->BindAction<FChangeSprint>("Sprint", EInputEvent::IE_Pressed, this, &AOPlayerController::ChangeSprint, true);
-    InputComponent->BindAction<FChangeSprint>("Sprint", EInputEvent::IE_Released, this, &AOPlayerController::ChangeSprint, false);
+	Super::SetupInputComponent();
+	InputComponent->BindAxis("MoveForward", this, &AOPlayerController::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &AOPlayerController::MoveRight);
+	InputComponent->BindAxis("Turn", this, &AOPlayerController::Turn);
+	InputComponent->BindAxis("LookUp", this, &AOPlayerController::LookUp);
+	InputComponent->BindAxis("TurnAtRate", this, &AOPlayerController::TurnAtRate);
+	InputComponent->BindAxis("LookUpAtRate", this, &AOPlayerController::LookUpAtRate);
+	InputComponent->BindAxis("SwimRight", this, &AOPlayerController::MoveSwimRight);
+	InputComponent->BindAxis("SwimUp", this, &AOPlayerController::MoveSwimUp);
+	InputComponent->BindAxis("SwimForward", this, &AOPlayerController::MoveSwimForward);
 	InputComponent->BindAxis("ClimbLadder", this, &AOPlayerController::ClimbLadder);
 	InputComponent->BindAction("Mantle", EInputEvent::IE_Pressed, this, &AOPlayerController::Mantle);
 	InputComponent->BindAction("InteractionWithLadder", EInputEvent::IE_Pressed, this, &AOPlayerController::InteractionWithLadder);
+	InputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &AOPlayerController::Jump);
+	InputComponent->BindAction("Crawl", EInputEvent::IE_Pressed, this, &AOPlayerController::ChangeCrawlState);
+	InputComponent->BindAction("Crouch", EInputEvent::IE_Pressed, this, &AOPlayerController::ChangeCrouchState);
+	InputComponent->BindAction<FChangeSprint>("Sprint", EInputEvent::IE_Pressed, this, &AOPlayerController::ChangeSprint, true);
+	InputComponent->BindAction<FChangeSprint>("Sprint", EInputEvent::IE_Released, this, &AOPlayerController::ChangeSprint, false);
 	InputComponent->BindAction("Fire", EInputEvent::IE_Pressed, this, &AOPlayerController::StartFireWithWeapon);
 	InputComponent->BindAction("Fire", EInputEvent::IE_Released, this, &AOPlayerController::StopFire);
 	InputComponent->BindAction("NextWeapon", EInputEvent::IE_Pressed, this, &AOPlayerController::NextWeapon);
@@ -77,131 +76,131 @@ void AOPlayerController::SetupInputComponent()
 
 void AOPlayerController::ChangeSprint(bool bWantsToSprint)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->ChangeSprint(bWantsToSprint);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->ChangeSprint(bWantsToSprint);
+	}
 }
 
 void AOPlayerController::LookUp(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->LookUp(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->LookUp(Value);
+	}
 }
 
 void AOPlayerController::Turn(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->Turn(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->Turn(Value);
+	}
 }
 
 void AOPlayerController::MoveRight(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->MoveRight(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->MoveRight(Value);
+	}
 }
 
 void AOPlayerController::MoveForward(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->MoveForward(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->MoveForward(Value);
+	}
 }
 
 void AOPlayerController::LookUpAtRate(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->LookUpAtRate(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->LookUpAtRate(Value);
+	}
 }
 
 void AOPlayerController::TurnAtRate(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->TurnAtRate(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->TurnAtRate(Value);
+	}
 }
 
 void AOPlayerController::Jump()
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->Jump();
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->Jump();
+	}
 }
 
 void AOPlayerController::ChangeCrouchState()
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->ChangeCrouchState();
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->ChangeCrouchState();
+	}
 }
 
 void AOPlayerController::ChangeCrawlState()
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->ChangeCrawlState();
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->ChangeCrawlState();
+	}
 }
 
 void AOPlayerController::MoveSwimRight(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->MoveSwimRight(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->MoveSwimRight(Value);
+	}
 }
 
 void AOPlayerController::MoveSwimForward(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->MoveSwimForward(Value, LastSwimUpValue);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->MoveSwimForward(Value, LastSwimUpValue);
+	}
 }
 
 void AOPlayerController::MoveSwimUp(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        LastSwimUpValue = Value;
-        CachedBaseCharacter->MoveSwimUp(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		LastSwimUpValue = Value;
+		CachedBaseCharacter->MoveSwimUp(Value);
+	}
 }
 
 void AOPlayerController::Mantle()
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->Mantle();
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->Mantle();
+	}
 }
 
 void AOPlayerController::ClimbLadder(float Value)
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->ClimbLadder(Value);
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->ClimbLadder(Value);
+	}
 }
 
 void AOPlayerController::InteractionWithLadder()
 {
-    if (CachedBaseCharacter.IsValid())
-    {
-        CachedBaseCharacter->InteractionWithLadder();
-    }
+	if (CachedBaseCharacter.IsValid())
+	{
+		CachedBaseCharacter->InteractionWithLadder();
+	}
 }
 
 void AOPlayerController::StartFireWithWeapon()
--- Source/Origin/Characters/Controllers/OPlayerController.h
+++ Source/Origin/Characters/Controllers/OPlayerController.h
@@ -18,32 +18,32 @@ class ORIGIN_API AOPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
-    virtual void SetPawn(APawn* InPawn) override;
+	virtual void SetPawn(APawn* InPawn) override;
 	bool IsPressedAnyKeyForAction(const FName ActionName) const;
 	bool FirstPressedKeyForAction(const FName ActionName, FText& OutKeyName) const;
 
 protected:
-    virtual void SetupInputComponent() override;
-    
-private:
-    TWeakObjectPtr<class AOBaseCharacter> CachedBaseCharacter;
-    void ChangeSprint(bool bWantsToSprint);
-    void LookUp(float Value);
-    void Turn(float Value);
-    void MoveRight(float Value);
-    void MoveForward(float Value);
-    void LookUpAtRate(float Value);
-    void TurnAtRate(float Value);
-    void Jump();
-    void ChangeCrouchState();
-    void ChangeCrawlState();
-    void MoveSwimRight(float Value);
-    void MoveSwimForward(float Value);
-    void MoveSwimUp(float Value);
+	virtual void SetupInputComponent() override;
 
-    void Mantle();
-    void ClimbLadder(float Value);
-    void InteractionWithLadder();
+private:
+	TWeakObjectPtr<class AOBaseCharacter> CachedBaseCharacter;
+	void ChangeSprint(bool bWantsToSprint);
+	void LookUp(float Value);
+	void Turn(float Value);
+	void MoveRight(float Value);
+	void MoveForward(float Value);
+	void LookUpAtRate(float Value);
+	void TurnAtRate(float Value);
+	void Jump();
+	void ChangeCrouchState();
+	void ChangeCrawlState();
+	void MoveSwimRight(float Value);
+	void MoveSwimForward(float Value);
+	void MoveSwimUp(float Value);
+
+	void Mantle();
+	void ClimbLadder(float Value);
+	void InteractionWithLadder();
 
 	void StartFireWithWeapon();
 	void StopFire();
@@ -51,6 +51,6 @@ private:
 	void ReloadAmmo();
 	void NextWeaponIndex();
 
-    // gimbal lock for swim up and swim forward for y = 90. if rotate vector up to forward vector
-    float LastSwimUpValue = 0.f;
+	// gimbal lock for swim up and swim forward for y = 90. if rotate vector up to forward vector
+	float LastSwimUpValue = 0.f;
 };
