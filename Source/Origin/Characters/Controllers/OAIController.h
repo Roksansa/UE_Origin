f136c7dd451747cee2a8c1601858c137ac173287
Roksansa <roksan.girl@rambler.ru>
2022-02-11 15:08:51 +0700

    [AI][Map] Create classes for AI. Set properties for AI Character. Change player pawn to ai pawn.

---
+++ Source/Origin/Characters/Controllers/OAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "OAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORIGIN_API AOAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Origin/Characters/OAICharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/OBaseCharacter.h"
+#include "OAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class ORIGIN_API AOAICharacter : public AOBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
